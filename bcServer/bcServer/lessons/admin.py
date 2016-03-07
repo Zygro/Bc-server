e093eb9758aa6f44e7cd385561b8c6a292174ccb
zygro <michalsmolik2@gmail.com>
2016-03-08 00:14:34 +0100

    add rating

--- bcServer/bcServer/lessons/admin.py
+++ bcServer/bcServer/lessons/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Lesson, Submit
+from .models import Lesson, Submit, Rating
 # Register your models here.
 admin.site.register(Lesson)
 admin.site.register(Submit)
+admin.site.register(Rating)
--- bcServer/bcServer/lessons/models.py
+++ bcServer/bcServer/lessons/models.py
@@ -3,9 +3,25 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Avg
 # Create your models here.
 
+class Rating(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    lesson = models.ForeignKey('Lesson')
+    fun = models.IntegerField(
+        default=3,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+    difficulty = models.IntegerField(
+        default=3,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
 class Lesson(models.Model):
     name = models.CharField(max_length=256)
     problem = models.TextField()
@@ -14,6 +30,9 @@ class Lesson(models.Model):
     bad_solutions = models.IntegerField(default=0)
     number = models.IntegerField(default=1)
     optional = models.BooleanField(default=True)
+    correct_solution = models.FileField(null=True, blank=True)
+    #fun = models.IntegerField(default=Rating.objects.filter(lesson=id).aggregate(Avg('fun')))
+
     def __str__(self):
         return self.name
 
